34277a5664edd775970107a5e652f79379ee3ef1
reptily <47777868+reptily@users.noreply.github.com>
2019-02-22 10:46:34 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ const config = {
         host:"127.0.0.1",
         port:"3306",
         database:"test",
-        login:"reptily",
+        user:"reptily",
         password:"",
         debug:false
         };
